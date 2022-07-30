82b5c90fcb54e996753a4462a4ba0cd26ccf2ff5
Drew2134 <a.winchell1896@gmail.com>
2022-07-29 23:05:42 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function setMap() {
     var promises = [];
     promises.push(d3.csv("data/education_attainment.csv")); //load education tabular data
     promises.push(d3.json("data/states.topojson")); //load background states shapes
-    promises.push(d3.json("data/tracts.toposjon")); //load foreground tracts shapes
+    promises.push(d3.json("data/tracts.topojson")); //load foreground tracts shapes
     Promise.all(promises).then(callback);
 
     function callback(data) {
