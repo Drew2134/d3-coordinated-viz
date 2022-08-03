827045bf900241fa062a56d2566af6548ddfb294
Drew2134 <a.winchell1896@gmail.com>
2022-08-02 19:22:00 -0500

    Update

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ function setMap() {
         .center([0, 38.9072])
         .rotate([77.0369, 0])
         .parallels([35, 40])
-        .scale(500)
+        .scale(2000)
         .translate([width / 2, height / 2]);
 
     var path = d3.geoPath()
