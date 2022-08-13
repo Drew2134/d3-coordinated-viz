7913761f51f1660431592ee4f66d24c541f9b0f2
Drew2134 <a.winchell1896@gmail.com>
2022-08-13 14:01:36 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -120,7 +120,7 @@
             })
             .attr("d", path)
             .style("fill", function(d){
-                console.log(colorScale(d.properties[expressed]))
+                console.log(d, d.properties, d.properties[expressed])
                 return colorScale(d.properties[expressed])
             });
     }
