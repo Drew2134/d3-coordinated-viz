33ca13f74c6b3ce2c1e41a5705e1bb7d23e164c1
Drew2134 <a.winchell1896@gmail.com>
2022-08-13 20:05:12 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -194,7 +194,7 @@
         for(var i = 0; i < csvData.length; i++){
             datum = {};
             datum.name = csvData[i].tract_name;
-            datum.value = csvData[i].proprties[expressed];
+            console.log(csvData[i].proprties);
             console.log(datum)
         }
 
