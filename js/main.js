b150189553ad757f1d5eaeaab11af37f29fc6b7a
Drew2134 <a.winchell1896@gmail.com>
2022-08-15 10:36:27 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -197,7 +197,7 @@
                         name: d => "",
                         title: d => "Census Tract: " + d.name + "\nCount: " + d.value,
                         value: d => d.value,
-                        colors: d => colorScale(d.properties[expressed]),
+                        groups: d => colorScale(d.properties[expressed]),
                         width: 800
                     });
 
