9e30b9668761126862012c9a3aae102e6bdfeb25
Drew2134 <a.winchell1896@gmail.com>
2022-08-15 09:20:45 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -193,6 +193,7 @@
         }
 
         var bubble = BubbleChart(data, {
+                        title: d => d.name,
                         value: d => d.value,
                         width: 800
                     });
