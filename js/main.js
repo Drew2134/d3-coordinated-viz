0bc4ec75f7fb87682381190ddc8190fe43dbcca6
Drew2134 <a.winchell1896@gmail.com>
2022-08-16 01:42:58 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -423,7 +423,7 @@
             .attr("fill", G ? d => color(G[d.data]) : fill == null ? "none" : fill)
             .attr("fill-opacity", fillOpacity)
             .attr("r", d => d.r)
-            .attr("class", D.name);
+            .attr("class", d => d.name);
     
         if (T) leaf.append("title")
             .text(d => T[d.data]);
