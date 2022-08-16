4794d21016755618836b6537ced6bbf88a405951
Drew2134 <a.winchell1896@gmail.com>
2022-08-15 23:58:12 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -108,6 +108,7 @@
         var domainArray = [];
         for (var i=0; i < data.length; i++){
             var val = parseFloat(data[i][expressed] / data[i]["total_population"]);
+            console.log(val)
             domainArray.push(val);
         };
 
